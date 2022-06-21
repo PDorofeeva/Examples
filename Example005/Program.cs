382b080f234836cf500e62e4fed527d2fe157008
Barashnya <polinadorofeeva418@gmail.com>
2022-06-21 23:37:25 +0300

    Добавили прекрасного Игоря

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Пожалуйста, введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "igor")
+{
+    Console.WriteLine("Hi, beauty :-D");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+    Console.WriteLine("Where is my sweet Igor?");
+}
