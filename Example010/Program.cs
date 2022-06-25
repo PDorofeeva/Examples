c38521021a561fcb02f5a47c8b8a9168fe20b515
Barashnya <polinadorofeeva418@gmail.com>
2022-06-25 13:45:12 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {2, 45, 5, 432, 3, 89, 50, 43, 7, 1, 34, 50};
+
+int n = array.Length;
+int index = 0;
+Console.WriteLine("Укажите искомое значение: ");
+int find = int.Parse(Console.ReadLine());
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Индекс, принадлежащий числу {find}: {index}");
+        break;
+    }
+
+    index++;
+}
+
