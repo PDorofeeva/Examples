de2cdd315ac74c08164c2ee88967a52364157150
Barashnya <polinadorofeeva418@gmail.com>
2022-06-28 13:43:21 +0300

    Example013, задача с Random number

---
+++ Example013/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+Random num = new Random();
+int number = num.Next(1, 1000);
+Console.WriteLine(number);
+int ThirdNumber = number % 10;
+int FirstNumber = number / 100;
+Console.Write(FirstNumber);
+Console.Write(ThirdNumber);
