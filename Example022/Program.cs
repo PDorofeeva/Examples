e086523a84d360f4350c1507f68fc054b323db03
Barashnya <polinadorofeeva418@gmail.com>
2022-07-04 20:27:37 +0300

    Example022

---
+++ Example022/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+int GetNumber()
+{
+    int result = 0;
+
+    while (true)
+    {
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите орректное числою");
+        }
+    }
+    return result;
+}
+
+int GetSum(int number)
+{
+    int result = 0;
+    for (int i = 1; 1 <= number; i++)
+    {
+        result += 1;
+    }
+    return result;
+}
+
+int number = GetNumber();
+int result = GetSum(number);
+
+Console.WriteLine(result);
