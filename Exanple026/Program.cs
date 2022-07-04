9cbb5b0f0c9131da0c4849922104688b2597cd82
Barashnya <polinadorofeeva418@gmail.com>
2022-07-04 21:47:25 +0300

    Example026

---
+++ Exanple026/Program.cs
@@ -0,0 +1,33 @@
+﻿void PrintArray(int[] array)
+{
+    for(int i = 0; i< array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void PrintArrayForEach(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+
+var array = new int[8];
+Random rnd = new Random();
+
+for(int i=0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1,10);
+}
+
+Console.WriteLine("Вывод через цикл for: ");
+PrintArray(array);
+Console.WriteLine();
+
+
+Console.WriteLine("Вывод через цикл foreach: ");
+PrintArrayForEach(array);
+
