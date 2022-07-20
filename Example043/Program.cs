bd75949ad1f57e2141735491ca6f9f18d3b62dfc
Barashnya <polinadorofeeva418@gmail.com>
2022-07-21 00:19:03 +0300

    Example043

---
+++ Example043/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+Задача 57: Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+входные данные: { 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string error = "Введите корректное число, пожалуйста!";
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result)) // int выводиm
+        {
+            break;
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine(error);
+        }
+    }
+
+    return result;
+}
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random yuy = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+        matrix[i, j] = yuy.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindNumber(int[,] matrix, int number) // Ищет в двумерном массиве определённое число
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i,j] == number)
+            {
+                count++;
+            }
+        }
+    }
+    Console.WriteLine($"Элемент {number} встречается в количестве {count}.");
+}
+
+
+
+int FindNumbers = GetNumber("Print your desired number: ");
+int m = GetNumber("Print m: ");
+int n = GetNumber("Print n: ");
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+FindNumber(matrix,FindNumbers);
